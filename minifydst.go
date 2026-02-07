080237d2787fc157ed9c027e99a5e49a4d3ef3ef
runoneall <runoobsteve@hotmail.com>
2026-02-07 18:19:41 +0800

    minify option

--- minifydst.go
+++ minifydst.go
@@ -13,9 +13,25 @@ import (
 
 func minifydst(file string, src io.Reader) {
 	m := minify.New()
-	m.AddFunc("text/html", html.Minify)
-	m.AddFunc("text/css", css.Minify)
-	m.AddFunc("application/javascript", js.Minify)
+
+	m.Add("text/html", &html.Minifier{
+		KeepComments:            false,
+		KeepConditionalComments: false,
+		KeepSpecialComments:     false,
+		KeepDefaultAttrVals:     false,
+		KeepDocumentTags:        false,
+		KeepEndTags:             false,
+		KeepQuotes:              false,
+		KeepWhitespace:          false,
+	})
+
+	m.Add("text/css", &css.Minifier{
+		Inline: true,
+	})
+
+	m.Add("application/javascript", &js.Minifier{
+		KeepVarNames: false,
+	})
 
 	dst, err := os.Create(file + ".min.html")
 	if err != nil {
